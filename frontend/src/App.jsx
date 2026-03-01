ea14070a30cfb7537a660c77fd93d6a53483bb0a
david598Uni <molaresdavid@gmail.com>
2026-03-01 03:29:11 +0100

    añadido icono

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -144,7 +144,7 @@ function App() {
       <header className="top-banner">
         <div className="top-banner__brand">
           <span className="top-banner__icon">
-            <HiShieldCheck size={28} className="text-accent" />
+            <img src="../public/excalibur.png" alt="Logo" className="top-banner__logo-img" />
           </span>
           <h1 className="top-banner__title">ExcaliSearch</h1>
         </div>
